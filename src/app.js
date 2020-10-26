da38f13f203e731fe7a3cb4c8a9dac6e239ec6a0
Yannick <khalfouni.yannick@gmail.com>
2020-10-26 23:53:04 +0100

    feat(): test version webpack

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import 'bootstrap';
+import _ from 'lodash';
+import './scss/app.scss';
+
+function component() {
+    console.log('log');
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
